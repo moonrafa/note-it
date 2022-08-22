2ac36d958c0bca12a95558b3499040bec42f2c6e
Rafaelly Rodrigues <rrafasrodrigues@gmail.com>
2022-08-22 20:42:38 -0300

    auth added

--- client/main.js
+++ client/main.js
@@ -1,6 +1,12 @@
 import { Template } from 'meteor/templating'
 import './main.html'
 import { Notes } from '../lib/collections.js'
+import { Accounts } from 'meteor/accounts-base'
+
+//accounts config
+Accounts.ui.config({
+  passwordSignupFields: 'USERNAME_ONLY'
+})
 
 Template.body.helpers({
   /*  notes: [{ text: 'My note 1' }, { text: 'My note 2' }, { text: 'My note 3' }]*/
@@ -16,12 +22,26 @@ Template.add.events({
     const target = e.target
     const text = target.text.value
 
-    //insert into the collection
-    Notes.insert({ text, createdAt: new Date() })
+    /*insert into the collection
+    Notes.insert({
+      text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    })*/
+    Meteor.insert('note.insert', text)
     //clear form
     target.text.value = ''
     //close modal
 
     $('#addModal').modal('close')
+  }
+})
+
+Template.note.events({
+  'click .delete-note': function () {
+    //Notes.remove(this._id)
+    Meteor.call('note.remove', this)
+    return false
   }
 })
